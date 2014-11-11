4204d969a7a70d7781ddf848d779e6a4ba58422f
Ricardo <ricardochampa@gmail.com>
2014-11-11 01:45:59 +0100

    prod

--- subastas/constants.py
+++ subastas/constants.py
@@ -1,4 +1,4 @@
 import os
-#REDIS_URL = os.environ['REDISCLOUD_URL']
-REDIS_URL = '127.0.0.1:6379'
+REDIS_URL = os.environ['REDISCLOUD_URL']
+#REDIS_URL = '127.0.0.1:6379'
 REDIS_CHANNEL = 'auction'
